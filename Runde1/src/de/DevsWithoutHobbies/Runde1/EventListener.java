4ff9cab139615400d2867540cc761b051bfed648
Noah Peeters <noah.peeters@icloud.com>
2016-07-16 13:02:18 +0200

    Gives item to a player

--- Runde1/src/de/DevsWithoutHobbies/Runde1/EventListener.java
+++ Runde1/src/de/DevsWithoutHobbies/Runde1/EventListener.java
@@ -1,11 +1,30 @@
 package de.DevsWithoutHobbies.Runde1;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class EventListener implements Listener {
     private Zauberkrieg plugin;
 
-    public EventListener(Zauberkrieg instance){
+    public EventListener(Zauberkrieg instance) {
         plugin = instance;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        plugin.getLogger().info("Welcome to the server");
+        Player p = e.getPlayer();
+        p.sendMessage(ChatColor.RED + "Welcome to the server");
+        ItemStack item = new ItemStack(Material.INK_SACK, 1, (short)2);
+        ItemMeta im = item.getItemMeta();
+        im.setDisplayName("Awesome effect");
+        item.setItemMeta(im);
+        p.getInventory().addItem(item);
     }
 }
